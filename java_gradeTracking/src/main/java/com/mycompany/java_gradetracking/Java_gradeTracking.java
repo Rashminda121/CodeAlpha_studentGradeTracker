1a871f816e507de11b3cddf0cc1552ab95a29d0d
Rashminda <rashminda121@gmail.com>
2024-02-14 20:08:27 +0530

    Add files via upload

---
+++ java_gradeTracking/src/main/java/com/mycompany/java_gradetracking/Java_gradeTracking.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.java_gradetracking;
+
+/**
+ *
+ * @author Rashminda
+ */
+public class Java_gradeTracking {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
