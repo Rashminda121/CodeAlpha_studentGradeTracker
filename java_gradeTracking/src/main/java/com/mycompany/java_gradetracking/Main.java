67e3b53eb658b6d6a4d97811c834ca87ec999319
Rashminda <rashminda121@gmail.com>
2024-02-14 20:10:30 +0530

    added main java

---
+++ java_gradeTracking/src/main/java/com/mycompany/java_gradetracking/Main.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.java_gradetracking;
+
+/**
+ *
+ * @author Rashminda
+ */
+public class Main {
+    
+}
